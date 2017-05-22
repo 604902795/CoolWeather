de9757129535baad714fad76fb39025885bc7943
kingdee_ludi <604902795@qq.com>
2017-05-22 15:38:41 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/aqi.java
@@ -0,0 +1,12 @@
+package android.coolweather.com.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/5/22.
+ */
+public class AQI {
+    public AQCity city;
+    public class AQCity{
+        public String aqi;
+        public String pm25;
+    }
+}
